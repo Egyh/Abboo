460e6397ba624a6bb4b151a6e93db13460fd8b8c
Egyh <124222570+Egyh@users.noreply.github.com>
2023-04-08 07:40:22 +0000

    userとarticleコントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_08_051659) do
+ActiveRecord::Schema.define(version: 2023_04_08_051942) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2023_04_08_051659) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name_tag"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
