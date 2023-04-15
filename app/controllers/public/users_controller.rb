2cfcf3dcab83b5af4ba2bbdca60a949b3ed9acc1
Egyh <124222570+Egyh@users.noreply.github.com>
2023-04-15 03:45:58 +0000

    ゲストログインの実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,6 +2,7 @@ class Public::UsersController < ApplicationController
   
   before_action :is_matching_login_user, only: [:edit, :update]
   before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_guest_user, only: [:edit]
   
   def index
     @users = User.page(params[:page])
@@ -58,4 +59,11 @@ class Public::UsersController < ApplicationController
     redirect_to user_path(current_user)
     end
   end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 end
