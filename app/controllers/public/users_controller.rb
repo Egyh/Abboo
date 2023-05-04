d43ac03b007f0de468e85d4ddef13d18f129213f
Egyh <124222570+Egyh@users.noreply.github.com>
2023-05-04 11:30:19 +0000

    Vision APIの修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -12,7 +12,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.all
+    @articles = @user.articles.all.order(created_at: :desc)
     @articles = Kaminari.paginate_array(@articles).page(params[:page]).per(8)
     @article = Article.new
   end
