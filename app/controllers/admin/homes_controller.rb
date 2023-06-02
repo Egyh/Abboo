e66bbde38d079d5191cc17304cf505678a894a4e
Egyh <124222570+Egyh@users.noreply.github.com>
2023-06-02 05:21:04 +0000

    admin側の投稿数の設定

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,12 @@
 class Admin::HomesController < ApplicationController
+before_action :authenticate_admin!
 
 def top
     @articles = Article.all
+    @today_article =  @articles.created_today
+    @yesterday_article = @articles.created_yesterday
+    @this_week_article = @articles.created_this_week
+    @last_week_article = @articles.created_last_week
 
 end
 
